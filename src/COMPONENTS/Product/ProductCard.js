25f850dc41026750759b34f60e6e130ef9383472
Bishal Chansi <chansibishal55@gmail.com>
2023-06-12 10:59:10 +0545

    Responsiveness

--- src/COMPONENTS/Product/ProductCard.js
+++ src/COMPONENTS/Product/ProductCard.js
@@ -106,7 +106,7 @@ const ProductCard = ({ data }) => {
         :
         <div className='addbtn'>
         <Link 
-        to={`/product/${data.id}`}
+        to={`/product/${data.ProductId}`}
         >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
         stroke="currentColor" className="w-6 h-6"
--- src/COMPONENTS/Product/ProductSlider.js
+++ src/COMPONENTS/Product/ProductSlider.js
@@ -5,22 +5,32 @@ import ProductCard from './ProductCard'
 import './ProductSlider.css'
 const ProductSlider = ({products,categoryname}) => {
     const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 5,
-          slidesToSlide: 3 // optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 4,
-          slidesToSlide: 2 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 3,
-          slidesToSlide: 1 // optional, default to 1.
-        }
-      };
+      display6: {
+        breakpoint: { max: 2000, min: 1700 },
+        items: 6
+    },
+    display5:{
+        breakpoint: { max: 1700, min: 1200 },
+        items: 5
+    },
+
+    display4:{
+        breakpoint: { max: 1200, min: 900 },
+        items: 4
+    },
+    display3:{
+        breakpoint: { max: 900, min: 600 },
+        items: 3
+    },
+    display1:{
+        breakpoint: { max: 600, min: 500 },
+        items: 2
+    },
+    display0:{
+        breakpoint: { max: 500, min: 0 },
+        items: 1
+    }
+};
   return (
     <div className='productsliderout'>
         <h1>{categoryname}</h1>
